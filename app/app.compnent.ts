9f2855d726d0179f1a45e6808b8103875c218ef1
Derek Mauildin <derek.e.mauldin@gmail.com>
2016-04-24 14:10:35 -0600

    added appfile added templates added spaash-page.html added app.component.ts

--- app/app.compnent.ts
+++ app/app.compnent.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'my-app',
-	template: "templates/splash-page.html"
+	template: 'app/templates/splash-page.html'
 })
 export class AppComponent { }
